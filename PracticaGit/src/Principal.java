1e812532661c8ad806df9f4163c6c0680222d442
Gerar <gerardojaviermiranda@ciudadescolarfp.es>
2022-04-22 13:26:04 +0200

    intanciar objeto en master, paso 8

--- PracticaGit/src/Principal.java
+++ PracticaGit/src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("“Listado de alumnos 1o DAW.");
+		System.out.println("Alumno 1:");
+		Alumno al1 = new Alumno("ricardo","hernan","123456789X");
 	}
 
 }
